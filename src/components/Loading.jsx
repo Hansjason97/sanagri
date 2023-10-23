a8596c751b667ccdb3e17692bbc2c4b4b6e0721c
Hansjason97 <hansadiogo@gmail.com>
2023-10-23 10:24:06 +0100

    updated loading and main image

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Loading() {
   return (
-    <div>Loading</div>
+    <div className='w-[100vw] h-[100vh] bg-white flex justify-center items-center'>
+      <img src="/sanagriLogo.svg" alt="logo Sanagri" className='w-[120px] h-[120px]' />
+    </div>
   )
 }
 
